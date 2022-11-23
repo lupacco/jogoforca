5f31172f47424814b694b157607b0219fc8b0084
lupacco <pagotolc@gmail.com>
2022-11-22 23:36:38 -0300

    Gerando letras vazias com base na escolha de uma palavra aleatória OK

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,3 +1,6 @@
+import palavras from "../palavras"
+import { useState } from "react"
+
 const gameSteps = [
     "assets/forca0.png",
     "assets/forca1.png",
@@ -7,16 +10,45 @@ const gameSteps = [
     "assets/forca5.png",
     "assets/forca6.png"
 ]
-
 export default function Jogo(){
+    const [word, setWord] = useState('')
+    const [wordToBeFilled, setWordToBeFilled] = useState('')
+
+    function generateRandom(maxLimit=palavras.length-1){
+        let rand = Math.random()*maxLimit
+        rand = Math.floor(rand)
+        
+        return rand
+    }
+
+    function chooseWord(){
+        let wordIndex = generateRandom()
+        setWord(palavras[wordIndex])
+        console.log(word)
+        createEmptyWord(word)
+    }
+
+    function createEmptyWord(wordToGuess){
+        let wordSize = wordToGuess.length
+        let dots = ""
+        for(let i=0;i<wordSize;i++){
+            if(i === wordSize-1){
+                dots += "_"
+            }else{
+                dots += "_ "
+            }
+        }
+        setWordToBeFilled(dots)
+    }
+
     return(
         <main>
             <div className="left">
                 <img alt="" src={gameSteps[5]}></img>
             </div>
             <div className="right">
-                <button>Escolher Palavra</button>
-                <p>a _ _ _ _ _ _ _ _ _ _</p>
+                <button onClick={chooseWord}>Escolher Palavra</button>
+                <p>{wordToBeFilled}</p>
             </div>
         </main>
     )
